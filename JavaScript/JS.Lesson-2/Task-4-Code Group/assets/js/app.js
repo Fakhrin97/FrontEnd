8cd82ba031657abcaa6049687eb7b8b38f8347c7
Fakhrin97 <fakhrinya@code.edu.az>
2022-07-04 15:58:35 +0400

    JS-Functions

--- JavaScript/JS.Lesson-2/Task-4-Code Group/assets/js/app.js
+++ JavaScript/JS.Lesson-2/Task-4-Code Group/assets/js/app.js
@@ -3,18 +3,19 @@
 function Check(){
     var text=document.getElementById("text").value
     var no=text.slice(-3,-2)
+    
     console.log(no)
     if(no==1){
         document.body.style.background='yellow'
-        document.getElementById('schedule').innerHTML='Morning Group'
+        document.getElementById('schedule').innerHTML=text
     }
     else if(no==2){
         document.body.style.background='red'
-        document.getElementById('schedule').innerHTML='Afternun Grioup'
+        document.getElementById('schedule').innerHTML=text
     }
     else if(no==3){
         document.body.style.background='black'
-        document.getElementById('schedule').innerHTML='Night Group'
+        document.getElementById('schedule').innerHTML=text
         document.getElementById('schedule').style.color='white'
 
 
